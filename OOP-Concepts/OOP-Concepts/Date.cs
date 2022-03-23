e70c0a9bc157d2a1e04db7301ec6386e83df2166
gonzaloog88 <gonzaloog88@gmail.com>
2022-03-23 02:28:30 -0600

    finished Date class

---
+++ OOP-Concepts/OOP-Concepts/Date.cs
@@ -0,0 +1,76 @@
+﻿namespace OOP_Concepts
+{
+    public class Date
+    {
+        private int _day;
+        private int _month;
+        private int _year;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month, day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month.Equals(2) && day.Equals(29) && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException("Invalid day");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            //return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            throw new MonthException("Invalid month");
+        }
+
+        public override string ToString()
+        {
+            // interpolación de strings
+            return $"{_year}/{_month:00}/{_day:00}";
+        }
+    }
+}
---
+++ OOP-Concepts/OOP-Concepts/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Concepts;
+
+Console.WriteLine("OOP Concepts");
+Console.WriteLine("============");
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
